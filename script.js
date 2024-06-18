a74b227792a88c2dfe51ceaf9cdbf5be1f720192
Milene Naiara Rebinski <108832930+MileneNaiara@users.noreply.github.com>
2024-06-18 11:27:41 -0300

    18-06

---
+++ script.js
@@ -0,0 +1,13 @@
+const caixaPrincipal = document.querySelector('.caixa-principal');
+const caixaPerguntas = document.querySelector('.caixa-perguntas');
+const caixaAlternativas = document.querySelector('.caixa-alternativas');
+const caixaResultados = document.querySelector('.caixa-resultados');
+const textoResultado = document.querySelector('.texto-resultado');
+const lista = [item1, item2]
+const perguntas = []
+const lapis = {
+   tamanho: 20,
+   tipo: 'HB',
+   cor: 'Grafite',
+   temBorrachaAtras: false
+}
