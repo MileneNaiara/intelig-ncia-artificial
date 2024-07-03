3d95e3084084c130b97eac0eaf606c7b10d1cbda
Milene Naiara Rebinski <108832930+MileneNaiara@users.noreply.github.com>
2024-07-03 11:44:41 -0300

    03-07

--- script.js
+++ script.js
@@ -9,11 +9,11 @@ const perguntas = [
       enunciado: "Assim que saiu da escola Gabriel se depara com uma nova tecnologia, um chat que consegue responder todas as dúvidas que uma pessoa pode ter, o chat também gera imagens e áudios hiper-realistas. Qual o primeiro pensamento de Gabriel?",
       alternativas: [
         {
-         texto:"Isso é assustador!",
+         texto: "Isso é assustador!",
          afirmação: "Afirmação 1"
         },
         {
-         texto:"Isso é maravilhoso!",
+         texto: "Isso é maravilhoso!",
          afirmação: "Afirmação 2"
         },
         ],
@@ -22,11 +22,11 @@ const perguntas = [
       enunciado: "Com a descoberta desta tecnologia uma professora de tecnologia da escola decidiu fazer uma sequência de aulas sobre IA. No fim de uma aula ela pede que Gabriel escreva um trabalho sobre o uso de tecnologia em sala de aula. Qual atitude Gabriel toma?",
       alternativas: [
         {
-         texto:"Utiliza uma ferramenta de busca na internet que utiliza IA para que ela ajude a encontrar informações relevantes para o trabalho e explique numa linguagem que facilite o entendimento.",
+         texto: "Utiliza uma ferramenta de busca na internet que utiliza IA para que ela ajude a encontrar informações relevantes para o trabalho e explique numa linguagem que facilite o entendimento.",
          afirmação: "Afirmação 1"
         },
         {
-         texto:"Escreve o trabalho com base nas conversas que teve com colegas, algumas pesquisas na internet e conhecimentos próprios sobre o tema.",
+         texto: "Escreve o trabalho com base nas conversas que teve com colegas, algumas pesquisas na internet e conhecimentos próprios sobre o tema.",
          afirmação: "Afirmação 2"
         },
         ],
@@ -35,11 +35,11 @@ const perguntas = [
       enunciado: "Depois que Gabriel escreveu o trabalho, teve uma discussão sobre o impacto da IA no trabalho do futuro o que Gabriel faz:",
       alternativas: [
         {
-         texto:"Defende a ideia de que a IA pode criar novas oportunidades de emprego e melhorar habilidades humanas",
+         texto: "Defende a ideia de que a IA pode criar novas oportunidades de emprego e melhorar habilidades humanas",
          afirmação: "Afirmação 1"
         },
         {
-         texto:"Me preocupo com as pessoas que perderão seus empregos para máquinas e defendem a importância de proteger os trabalhadores.",
+         texto: "Me preocupo com as pessoas que perderão seus empregos para máquinas e defendem a importância de proteger os trabalhadores.",
          afirmação: "Afirmação 2"
         },
         ],
@@ -48,11 +48,11 @@ const perguntas = [
     enunciado: "Ao final da discussão, Gabriel precisou criar uma imagem no computador que representasse o que pensa sobre IA. E agora?",
     alternativas:[
         {
-        texto:"Criar uma imagem utilizando um gerador de imagem de IA.", 
+        texto: "Criar uma imagem utilizando um gerador de imagem de IA.", 
         afirmação: "Afirmação 1"
         },
         {
-        texto:"Criar uma imagem utilizando uma plataforma de design como o Paint.",
+        texto: "Criar uma imagem utilizando uma plataforma de design como o Paint.",
         afirmação: "Afirmação 2"
         },
     ],
