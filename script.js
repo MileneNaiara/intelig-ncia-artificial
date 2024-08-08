126f6466e0ba0fb6253350c62d7c63f535bf5a9a
Milene Naiara Rebinski <108832930+MileneNaiara@users.noreply.github.com>
2024-08-08 08:16:48 -0300

    0808

--- script.js
+++ script.js
@@ -50,7 +50,7 @@ const perguntas = [
         {
         texto: "Criar uma imagem utilizando um gerador de imagem de IA.", 
         afirmacao: "Acelerou o processo de criação de trabalhos utilizando geradores de imagem e agora consegue ensinar pessoas que sentem dificuldades em desenhar manualmente como utilizar também!"
-        }
+        },
         {
         texto: "Criar uma imagem utilizando uma plataforma de design como o Paint.",
         afirmacao: "Notou também que muitas pessoas não sabem ainda utilizar as ferramentas tradicionais e decidiu compartilhar seus conhecimentos de design utilizando ferramentas de pintura digital para iniciantes."
